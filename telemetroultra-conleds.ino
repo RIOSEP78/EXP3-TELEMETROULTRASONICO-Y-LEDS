9a0421574da0728e4dc99b1932afb422c864d4b2
Katacoda Scenario <scenario@katacoda.com>
2020-09-03 16:52:05 +0000

    Se corrige código Led apagados fuera del rango operacional y comentarios nuevos

--- telemetroultra-conleds.ino
+++ telemetroultra-conleds.ino
@@ -1,93 +1,78 @@
-/*
-   ################
-   #   Lab15125   #
-   ################
- 
-   Este codigo tiene como objetivo dar muestra de un uso practico de:
-   Sensor ultrasonico ping (parallax)
+iint inches = 0;// declara variables en pulgadas 
+long cm = 0;  // declara variables en cm en Long para mayor número de decimales
 
-   Según la hoja de datos de Parallax para el PING))), 
-   hay 73,746 microsegundos por pulgada o 29,034 microsegundos por centimetro 
-   (es decir, el sonido viaja a 1130 pies (o 34442.4cm) por segundo). 
-   Este da la distancia recorrida por el ping, ida y vuelta, 
-   por lo que dividimos por 2 para obtener la distancia del obstáculo.
-   ver: 
-   https://www.parallax.com/sites/default/files/downloads/28015-PING-Sensor-Product-Guide-v2.0.pdf
-        [En el PDF: TO_IN = 73_746' Inches ; TO_CM = 29_034' Centimeters ]
+/*Se define una función readUltrasonicDistance al inicio para llamarla posteriormente 
+en la función loop es del tipo Long par aumentar precisión en los decimales */
 
-   El circuito:
-     * +V conectado a sensor PING))) en +5V
-     * GND conectado a sensor PING))) en GND (ground)
-     * SIG conectado a sensor PING))) en pin digital 7
-     * LED conectado a pin 9 (PWM)
 
-   Funcion
-   readUltrasonicDistance(int triggerPin, int echoPin): Referencia obtenida de sensor ultrasonico tinkercad.com
-*/
-int inches = 0;//declara variables pulgadas
-int cm = 0;// decalara vaiables en centímetos 
-
-/*Se define una función readUltrasonicDistance al inicio 
-para poder llamarla en el loop es del tipo long para 
-aumentar la presición y tener mayor cantidad de decimsles 
-Triger emite frecuencia y eco recibe mismo terminal SIG*/
+/*función readUltrasonicDistance calcula la distancia en cm hasta el objeto 
+trig emite una frecuencia en HZ y Eco la recibe en el mismo terminal llamado SIG */
 
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
-  pinMode(triggerPin, OUTPUT);  // Inicializar LOW para limpiar trigger por 2 microsegundos
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Inicializar trigger en HIGH por 8 microsegundos para comenzar
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
+  pinMode(triggerPin, OUTPUT);  //TRIG COMO SEMÑAL DE SALIDA 
+  digitalWrite(triggerPin, LOW);// TRIG EN ESTADO APAGADO
+  delayMicroseconds(2); // SE ESPERA UN TIEMPO DE 2 ms 
+  digitalWrite(triggerPin, HIGH);// Se enciende TRIG 
+  delayMicroseconds(10); //10 ms encendido Trig
+  digitalWrite(triggerPin, LOW);// se Apagda Triig
+  pinMode(echoPin, INPUT);// Se enciende ECO 
   // lectura de pin echo con el retorno de la señal
-  return pulseIn(echoPin, HIGH);
+  return pulseIn(echoPin, HIGH); //función devuelve valor de tipo long
 }
 
-
-/*FUNCIÓN SETUP QUE SE EJECUTA 1 SOLA VEZ Y CONFIGURA EL SISTEMA */
-
+/*Función Setup que se ejecuta 1 sola vez y configura el sistema */
 void setup()
 {
-  Serial.begin(9600);// comunicación serial 9600 caracteres por segundo
-  pinMode(8, OUTPUT); //salida LED VERDE
-  pinMode(7, OUTPUT); //salida LED AMARILLO
-  pinMode(4, OUTPUT); //salida LED ROJO
+  
+  Serial.begin(9600);// comunicación serial 9600 baudios(9600 caracteres por segundo)
+  
+  pinMode(8, OUTPUT); //LED VERDE COMO SALIDA EN PIN 8
+  pinMode(7, OUTPUT); //LED AMARILLO COMO SALIDA EN PIN 7
+  pinMode(4, OUTPUT);  // LED ROJO COMO SALIDA EN PIN 4
 }
 
-/*FUNCIÓN LOOP SE EJECUTA EN FORMA CICLICA */
-void loop()
-{ 
- //FLOAT VARIABLE NÚMERO ENTERO CON 2 DECIMALES 
-  float distancia = 0.01723 * readUltrasonicDistance(2, 2);
-  /*CONDICIONES PARA DIFERENTES DISTANCIAS*/
-  if((distancia < 336) && (distancia >= 200)) 
-  {
-    digitalWrite(4, HIGH); //ENCIENDE LED ROJO
-    digitalWrite(7, LOW); // LED AMARILLO APAGADO
-    digitalWrite(8, LOW); // LED VERDE APAGADO
+/**Función Loop se ejecuta en forma cíclica */
+void loop()  
+{
+   /*float variable número entero con 2 decimales*/
+  float distancia = 0.01723*readUltrasonicDistance(2, 2);
+  
+  /* Condiciones para diferentes distancias */
+  
+  if(distancia > 336)  // LEDS APAGADOS FUERA DEL RANGO OPERACIONAL
+   {
+  digitalWrite(4, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);    
+  } 
+ 
+ if((distancia <= 336) && (distancia >= 200)) {
+  /*SÓLO ENCIENDE LED ROJO */
+  digitalWrite(4, HIGH); 
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
   } 
   else {
-    digitalWrite(4, LOW); //LED ROJO APAGADO
+   digitalWrite(4, LOW); 
   }
-  /*CONDICION PARA CIERTA DISTANCIA */
   if((distancia < 200) && (distancia >= 100)) {
-    digitalWrite(7, HIGH);//LED AMARILLO ENCENDIDO
+    
+    digitalWrite(7, HIGH);//SÓLO ENCIENDE LED AMARILLO 
   } 
   else {
-    digitalWrite(7, LOW); //LED AMARILLO APAGADO
+    digitalWrite(7, LOW); 
   }
-  /*CONDICIÓN PARA CIERTA DISTANCIA*/
-  if(distancia < 100) {
-    digitalWrite(8, HIGH);//LED VERDE ENCIENDE A UNA DISTANCIA MENOR A 100 cm
+  if(distancia < 100 ) {
+    digitalWrite(8, HIGH);// SÓLO ENCIENDE LED VERDE
   } 
   else {
-    digitalWrite(8, LOW); //LED VERDE APAGADO
+    digitalWrite(8, LOW); 
   }
-  /*SE COMPARA MONITOR SERIE CON VALOR VISUAL DEL SENSOR SUS RANGO*/
-  Serial.print("distancia: "); //IMPRIMIR EN MONITOR SERIE
-  Serial.println(distancia+1.8); // IMPRIME VALOR DE DISTANCIA 
-  delay(10); // Delay a little bit to improve simulation performance
+                                      
+  /* COMPARACIÓN DE RANGO ENTRE MONITOR EN SERIE Y VALOR VISUAL DEL SENSOR*/                                    
+  Serial.print("distancia: ");// IMPRIMIR EN MONITOR EN SERIE 
+  Serial.println(distancia+1.8);//IMPRIME VALOR DISTANCIA "CALIBRADA"
+ 
+  delay(100); // esperar 100 ms
 }
